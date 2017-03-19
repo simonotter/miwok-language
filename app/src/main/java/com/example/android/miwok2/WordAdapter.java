f78e566f8c42e8005dfbc939fc180b516872c06b
Simon Otter <simonotter@gmail.com>
2017-03-19 14:16:48 +0000

    Add images to numbers, colours, family members and phrases activities using custom ArrayAdapter.

--- app/src/main/java/com/example/android/miwok2/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok2/WordAdapter.java
@@ -3,6 +3,7 @@ package com.example.android.miwok2;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,18 +13,23 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
+
 /**
  * Created by sotter on 15/03/2017.
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
+        mColorResourceId = colorResourceId;
     }
 
     @Override
@@ -49,7 +55,29 @@ public class WordAdapter extends ArrayAdapter<Word> {
         // Get the default word from the current Word object and set this text on the TextView
         defaultTextView.setText(currentWord.getDefaultTranslation());
 
-        // Return the whole list item layout (containing 2 TextViews)
+        // Find the ImageView in the list_item.xml layout with the ID image_view
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image_view);
+
+        // Check if the image from the current Word object exists
+        if (currentWord.hasImage()) {
+            // Get the image from the current Word object and set this image on the ImageView
+            imageView.setImageResource(currentWord.getmImageResourceId());
+
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_container);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+
+        // Return the whole list item layout (containing 2 TextViews and 1 ImageView)
         // so that it can be shown in the ListView
         return listItemView;
 
